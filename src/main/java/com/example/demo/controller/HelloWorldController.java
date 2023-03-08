803aa01d18ea385f69df16929b396d816aa92f3a
chaitanya reddy <s.c.reddy@globallogic.com>
2023-03-08 12:54:05 +0530

    dependency errors are resolved

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,26 +1,25 @@
-//package com.example.demo.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//import com.example.demo.Repository.OrderRepository;
-//import com.example.demo.model.Orders;
-//
-//import java.time.LocalDateTime;
-//
-//@RestController
-//public class HelloWorldController {
-//
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.time.LocalDateTime;
+
+@RestController
+public class HelloWorldController {
+
 //    @Autowired
 //    OrderRepository OrderRepository;
-//
-//    @GetMapping("/")
-//    public String greeting(){
-//        return "cloud build demo";
-//    }
+
+    @GetMapping("/")
+    public String greeting(){
+        return "cloud build demo";
+    }
 //    @PutMapping("/gadgets")
 //    public ResponseEntity<?> insertgadgets(@RequestBody Orders orders) {
 //        orders.SetOrder_id("ord01");
@@ -28,5 +27,5 @@
 //        orders.setTimestamp(LocalDateTime.now());
 //        return OrderRepository.save(orders);
 //    }
-//
-//}
+
+}
